319582ec0c5b6affb1aa341daca23d9dc34e7fe9
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-12-07 18:45:58 +0100

    comit

--- classes/banUser.php
+++ classes/banUser.php
@@ -1,18 +1,20 @@
 <?php
-    require_once 'Utilisateur.php';
-    require_once 'Admin.php';
+require_once 'Utilisateur.php';
+require_once 'Admin.php';
 
-    //verif Admin 'pierre' pour l'exemple
-    $admin = new Admin('Pierre', 'abcdef'); 
-    $admin->setBan($usernameToBan);
-    echo "L'utilisateur $usernameToBan a été banni.";
-    $bannedUsers = $admin->getBannedUsers();
+//verif Admin 'pierre' pour l'exemple
+$admin = new Admin('Pierre', 'abcdef'); 
 
-    echo "Liste des utilisateurs bannis:<br>";
-    foreach ($bannedUsers as $user) {
-        echo $user . "<br>";
-    }
-    header('Location: admin.php'); 
-    exit();
+$usernameToBan = $_POST['usernameToBan'];
 
+$admin->setBan($usernameToBan);
+
+echo "L'utilisateur $usernameToBan a été banni.";
+
+$bannedUsers = $admin->getBannedUsers();
+
+echo "Liste des utilisateurs bannis:<br>";
+foreach ($bannedUsers as $user) {
+    echo $user . "<br>";
+}
 ?>
